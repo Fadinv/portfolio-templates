d9ceef672d5139f36330f515a7cb3c23e1f60cdb
Fadinv <fadinvv95@gmail.com>
2021-01-10 21:41:06 +0300

    aasd
    asd

--- pages/Components/Description/Description.tsx
+++ pages/Components/Description/Description.tsx
@@ -1,32 +1,19 @@
 import React from 'react'
 import styles from './Description.module.css'
 
-const myDescription = {
-    clicker: {
-        src: 'https://github.com/Fadinv/clicker',
-        ps: 'Фоновую музыку я написал сам, поэтому дал вам возможность ее отключить :)',
-    },
-    cdlook: {
-        src: 'https://github.com/Fadinv/cdl',
-        ps: false
-    }
-}
-
 interface DescriptionProps {
     descriptionType: 'clicker' | 'cdlook'
 }
 
 const Description: React.FC<DescriptionProps> = ({descriptionType}) => {
 
-    const props = myDescription[descriptionType]
-    if (!props) return
-    if (!props.src) return
-
     return (
         <div className={styles.Description}>
             <h3>Ссылка на исходный код</h3>
-            <a href={props.src}>{props.src}</a>
-            {props.ps ? <p className={styles.Ps}>Ps. {props.ps}</p> : null}
+            <a href={descriptionType === 'clicker' ? 'https://github.com/Fadinv/clicker' : 'https://github.com/Fadinv/cdl'}>
+                {descriptionType === 'clicker' ? 'https://github.com/Fadinv/clicker' : 'https://github.com/Fadinv/cdl'}
+            </a>
+            {descriptionType === 'clicker' ? <p className={styles.Ps}>Ps. Фоновую музыку я написал сам, поэтому дал вам возможность ее отключить :)</p> : null}
         </div>
     )
 }
